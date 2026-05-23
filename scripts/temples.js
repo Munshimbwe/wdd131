41c13fde8b7e2245e707e53c3e1ebde9039551a3
Munshimbwe <bkafwanka@byupathway.edu>
2026-05-23 05:43:03 -0700

    updates added as per rubric

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,7 +4,17 @@ document.getElementById('last-modified').textContent = document.lastModified;
 const menuButton = document.getElementById('menu-btn');
 const navigationMenu = document.getElementById('nav-menu');
 
-menuButton.addEventListener('click', () => {
-  navigationMenu.classList.toggle('open');
-  menuButton.classList.toggle('open');
-});
+if (menuButton && navigationMenu) {
+  menuButton.addEventListener('click', () => {
+    navigationMenu.classList.toggle('open');
+    menuButton.classList.toggle('open');
+  });
+
+  const navLinks = navigationMenu.querySelectorAll('a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      navigationMenu.classList.remove('open');
+      menuButton.classList.remove('open');
+    });
+  });
+}
